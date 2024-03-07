fd70db5d7694503e48b5db364684250825e4cad0
Darshan <darshanpanchal557@gmail.com>
2024-03-08 00:30:38 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,21 @@ import Login from './pages/Login';
 import Payment from './pages/Payment';
 import { Routes, Route } from 'react-router-dom';
 import AdminPanel from './pages/AdminPanel';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <Home />
+            </>
+          }
+        />
+
         <Route
           path='/login'
           element={
