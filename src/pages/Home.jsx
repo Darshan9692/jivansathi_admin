adc1759cfcde0c7ca41cc8bb8285326c1a5a0a44
Darshan <darshanpanchal557@gmail.com>
2024-03-08 00:47:31 +0530

    Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div className="flex justify-center items-center h-screen bg-teal-500">
           <h1 className="text-3xl text-white">Welcome to Jeevansathi</h1>
+          <Link to="/admin">Admin Panel</Link>
         </div>
       )      
 }
