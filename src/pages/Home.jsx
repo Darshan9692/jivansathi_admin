130d36e9a38f9c313d887a8d43e7727aa11bfdb3
Darshan <darshanpanchal557@gmail.com>
2024-03-08 00:48:23 +0530

    Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
-    return (
-        <div className="flex justify-center items-center h-screen bg-teal-500">
-          <h1 className="text-3xl text-white">Welcome to Jeevansathi</h1>
-          <Link to="/admin">Admin Panel</Link>
-        </div>
-      )      
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-teal-500">
+      <h1 className="text-4xl text-white mb-6 font-bold">Welcome to Jeevansathi</h1>
+      <Link to="/admin" className="text-white text-lg underline hover:text-teal-300">Go to Admin Panel</Link>
+    </div>
+  )
 }
 
 export default Home
